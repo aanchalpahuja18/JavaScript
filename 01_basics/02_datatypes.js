b37e846f6269f098ab68ef0ce64a2893256aeb7c
aanchalpahuja18 <136963878+aanchalpahuja18@users.noreply.github.com>
2024-02-11 14:08:59 +0000

    datatypes in JS

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Aanchal")
+
+
+let name = "Aanchal"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint => used to store bigger values
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => variable is declared but not initialised
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined - is a type itself
+console.log(typeof null); // object - null is a object
