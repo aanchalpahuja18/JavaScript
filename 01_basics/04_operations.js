20942ecffeda51a4d8836cb6d6a0f1ed642f5e2e
aanchalpahuja18 <136963878+aanchalpahuja18@users.noreply.github.com>
2024-02-11 14:39:49 +0000

    Operations in JS

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,40 @@
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " aanchal"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); // output => 12
+// console.log(1 + "2"); // outout => 12
+// console.log("1" + 2 + 2); // output => 122
+// console.log(1 + 2 + "2"); // output => 32
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true); // outout => conversion into number = 1
+// console.log(+""); // output => conversion into number = 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+gameCounter ++;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
