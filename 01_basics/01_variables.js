ce9cedef161c38a4abfe0b5c63ff444ab076086a
aanchalpahuja18 <136963878+aanchalpahuja18@users.noreply.github.com>
2024-02-11 14:08:44 +0000

    variables in JS

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = "1234";
+let accountEmail = "aanchal@gmail.com";
+let accountCity = "Nagpur";
+
+accountState = "Maharashtra";
+
+let accountDetails;
+// accountId = "4213";
+
+console.log(accountId);
+
+console.table([accountId, accountCity, accountEmail, accountState, accountDetails])
+
+
+/*
+Summary : const is used to declare and initialize constant variables.
+Prefer not to use var
+because of issue in block scope and functional scope
+Use let & const in modern JS
+let allows for block scope & functional scope
+
+*/
