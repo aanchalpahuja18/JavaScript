65b8f0f3941e82ca1595bd1edf9f2210627a495d
aanchalpahuja18 <136963878+aanchalpahuja18@users.noreply.github.com>
2024-08-03 09:26:37 +0000

    Comments Added

--- 01_basics/05_comparison.js
+++ 01_basics/05_comparison.js
@@ -8,6 +8,8 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
+//String Datatypes & Number datatypes gives unpredictable results on comparison operators. 2 different datatypes when compared always gives unpredictable results.
+
 console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
