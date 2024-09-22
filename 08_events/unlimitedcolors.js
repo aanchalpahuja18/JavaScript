8d842f502166d3fc5b15e7a3f030d1cf09389061
aanchalpahuja18 <136963878+aanchalpahuja18@users.noreply.github.com>
2024-09-22 15:39:01 +0000

    Changes Made

--- 08_events/unlimitedcolors.js
+++ 08_events/unlimitedcolors.js
@@ -14,23 +14,38 @@ console.log(randomColor());
 
 
 let intervalId;
+let bgColor;
 const startChangingColor = function(){
     //here we first check that if the intervalId is null then only we want to start the process of setInterval & generating colors. 
     if(!intervalId){
-        intervalId = setInterval(changeColor, 500)
+        intervalId = setInterval(changeColor, 700)
     }
 
     function changeColor(){
-        document.body.style.backgroundColor = randomColor()
+       bgColor = document.body.style.backgroundColor = randomColor()
     }
 }
 
+
+function checkRightColor() {
+    if(bgColor == rightGuess){
+        alert("Congratulations!!! You won!!")
+    }
+    else{
+        alert("Better Luck next time!")
+    }
+}
 const stopChangingColor = function(){
     clearInterval(intervalId)
     //Here we are resetting our intervalId to null since we don't want it to retain its value of setInterval. Its just for better code optimization.
     intervalId = null
+    checkRightColor()
 }
 
 document.querySelector("#start").addEventListener("click", startChangingColor)
 
 document.querySelector("#stop").addEventListener("click", stopChangingColor)
+
+
+const rightGuess = document.querySelector('#color').style.backgroundColor = randomColor()
+console.log(rightGuess);
