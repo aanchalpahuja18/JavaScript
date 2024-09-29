fa0794e517f380c324c5ec6c2abbbab443fa8999
aanchalpahuja18 <136963878+aanchalpahuja18@users.noreply.github.com>
2024-09-29 08:39:11 +0000

    Info Added

--- 10_classes_and_oop/getters_setters.js
+++ 10_classes_and_oop/getters_setters.js
@@ -25,6 +25,7 @@ console.log(hitesh.email);
 
 
 //getters & setters are used to give a custom control to the dev that what he/she has to return when the value is used by the user.
+//If we want to give a specific access to the user then we specifically use gettters & setters. 
 //Also getters & setters are present in all the classes & since its functionality is not implemented it works on its default code. We have to use them as the name of the variable & they are methods inside which we implement our own logic.
 //In the setter method since the constructor is also setting up a value & a setter is also setting up a value for the same variable so for that an error occurs which tells that Maximum stack size occurred & a race condition is happening between both of the setter() & the constructor. So to avoid this condition we have to take a new variable & set a value of the variable inside it. This will be taken care by JS & the same variable name should be present inside the getter().
 //With getter() we have to compulsorily write the setter() as well.
