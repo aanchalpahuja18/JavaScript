2eeec2159c6f23ad7e41d27ba3be4a5c015d4f47
aanchalpahuja18 <136963878+aanchalpahuja18@users.noreply.github.com>
2024-09-29 08:38:59 +0000

    Info Added

--- 10_classes_and_oop/properties_get_set.js
+++ 10_classes_and_oop/properties_get_set.js
@@ -4,6 +4,7 @@ function User(email, password){
     this._email = email;
     this._password = password
 
+    //Below takes 3 parameters => 1. context into which we want to define the properties (which is this here) since defineProperty has the context pointing to global object or else pointing to a null context. 2. the name of the property into which we want to define our getters & setters. 3. getters & setters methods defines inside { }
     Object.defineProperty(this, 'email', {
         get: function(){
             return this._email.toUpperCase()
